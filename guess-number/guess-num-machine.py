1eab0365d54e95ccf28a92d0f4c35db1daa28494
ashfaque md <ashfaquemd2002@gmail.com>
2021-09-04 14:03:48 +0530

    adding guess number by machine

---
+++ guess-number/guess-num-machine.py
@@ -0,0 +1,20 @@
+# guess-num-machine.py
+# program for guessing the number machine
+
+import random
+
+def guess_num_mach(upper_limit):
+    lower_limit = 1
+    user_response = ''
+    while user_response != 'C':
+        gen_num = random.randint(lower_limit,upper_limit)
+        user_response = input(f'Is {gen_num} too High (h), too Low (l) or correct (c): ').upper()
+        if user_response == 'H':
+            upper_limit = gen_num - 1
+        elif user_response == 'L':
+            lower_limit = gen_num + 1
+    print(f'{gen_num} is correct ! Machine guessed it..')
+
+
+upper_limit = int(input('Enter the upper limit: '))
+guess_num_mach(upper_limit)
